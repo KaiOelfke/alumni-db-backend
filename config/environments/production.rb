b6d7b6475921e507d5b2a1583d6d9ca85910587d
Ahmed Issa <neuissa@gmail.com>
2015-03-17 19:42:18 +0100

    add email-settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,15 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options =
   { host: ENV['BACKEND_PRODUCTION_HOST'], port: 80 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'alumnieurope.org',
+    user_name:            'db@alumnieurope.org',
+    password:             'BP^46g4DjtuC2s,,P*yYyncaa',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 
 
 end
