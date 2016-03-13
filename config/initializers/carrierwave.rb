77fc528e133f8e2dd24ec240e6e114b57613453a
ahmedissa <neuissa@gmail.com>
2016-03-13 01:53:07 +0100

    fix aws envs

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,22 +1,23 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    region: 'eu-central-1'
-  }
+
 
   # For testing, upload files to local `tmp` folder.
   if Rails.env.test? || Rails.env.development?
     config.storage = :file
   else
     config.storage = :fog
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: 'eu-central-1'
+    }
+    config.fog_directory = ENV['S3_BUCKET']
   end
 
 
   # To let CarrierWave work on heroku
   config.cache_dir = "#{Rails.root}/tmp/uploads" 
-  config.fog_directory = ENV['S3_BUCKET']
 
 
   if Rails.env.test? 
