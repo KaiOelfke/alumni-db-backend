ab4247d91c53d56c424d0fc7809af379608e3021
ahmedissa <neuissa@gmail.com>
2016-03-13 01:42:36 +0100

    remove the tables of groups and memberships && add .env to gitignore && change the names of braintre env variables

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,19 +1,5 @@
-
-# Test and Development
-if Rails.env.test? || Rails.env.development?
-	Braintree::Configuration.environment = :sandbox
-	Braintree::Configuration.logger = Logger.new(STDOUT)
-	Braintree::Configuration.merchant_id = ENV['BRAINTREE_SANDBOX_MERCHANT_ID']
-	Braintree::Configuration.public_key = ENV['BRAINTREE_SANDBOX_PUBLIC_KEY']
-	Braintree::Configuration.private_key = ENV['BRAINTREE_SANDBOX_PRIVATE_KEY']
-
-# Production	
-else
-	Braintree::Configuration.environment = :production
-	Braintree::Configuration.logger = Logger.new(STDOUT)
-	Braintree::Configuration.merchant_id = ENV['BRAINTREE_PRODUCTION_MERCHANT_ID']
-	Braintree::Configuration.public_key = ENV['BRAINTREE_PRODUCTION_PUBLIC_KEY']
-	Braintree::Configuration.private_key = ENV['BRAINTREE_PRODUCTION_PRIVATE_KEY']
-end
-
-
+Braintree::Configuration.environment = ENV['BRAINTREE_ENV']
+Braintree::Configuration.logger = Logger.new(STDOUT)
+Braintree::Configuration.merchant_id = ENV['BRAINTREE_MERCHANT_ID']
+Braintree::Configuration.public_key = ENV['BRAINTREE_PUBLIC_KEY']
+Braintree::Configuration.private_key = ENV['BRAINTREE_PRIVATE_KEY']
