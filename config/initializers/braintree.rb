22b65882e44b060f1a460deb2b00ea810af7937b
ahmedissa <neuissa@gmail.com>
2016-03-13 01:47:21 +0100

    fix braintree env variable

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,4 +1,4 @@
-Braintree::Configuration.environment = ENV['BRAINTREE_ENV']
+Braintree::Configuration.environment = ENV['BRAINTREE_ENV'].to_sym
 Braintree::Configuration.logger = Logger.new(STDOUT)
 Braintree::Configuration.merchant_id = ENV['BRAINTREE_MERCHANT_ID']
 Braintree::Configuration.public_key = ENV['BRAINTREE_PUBLIC_KEY']
