476defbe1fb94cc77fe1eb17384492bb2ee4b25d
ahmedissa <neuissa@gmail.com>
2016-03-20 13:04:42 +0100

    add more tests for search

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -4,11 +4,13 @@ RSpec.describe SearchController, type: :controller do
 
     before(:each) do 
       @registered_user = FactoryGirl.create(:user, :registered, :confirmed_email, :personal_programm_data)
+      @registered_user_two = FactoryGirl.create(:user, :registered, :confirmed_email, :personal_programm_data_diff)
+
     end
 
     describe 'GET /search' do
 
-      it "should return 400 for accesing users if user has not completed his profile" do
+      it "should return 400 if the user doesn't provide text param" do
         auth_headers = @registered_user.create_new_auth_token
         request.headers.merge!(auth_headers)
 
@@ -18,7 +20,7 @@ RSpec.describe SearchController, type: :controller do
 
       end
 
-      it "should return 400 for accesing users if user has not completed his profile" do
+      it "should return 400 if the page param isn't number" do
         auth_headers = @registered_user.create_new_auth_token
         request.headers.merge!(auth_headers)
 
@@ -28,7 +30,7 @@ RSpec.describe SearchController, type: :controller do
 
       end
 
-      it "should return 400 for accesing users if user has not completed his profile" do
+      it "should return 400 if the page param is 0" do
         auth_headers = @registered_user.create_new_auth_token
         request.headers.merge!(auth_headers)
 
@@ -38,14 +40,26 @@ RSpec.describe SearchController, type: :controller do
 
       end
 
-      it "should return 200 for accesing users if user has not completed his profile" do
+      it "should return 200 and users who's first name is first_test" do
         auth_headers = @registered_user.create_new_auth_token
         request.headers.merge!(auth_headers)
 
-        get :search, {page: "1", :text => "kai"}, format: :json
+        get :search, {page: "1", :text => "first_test"}, format: :json
 
         expect(response.code).to eq "200"
-        expect(json.length).to be > 0
+        expect(json.length).to eq 2
+
+      end
+
+
+      it "should return 200 and users who's first name is first_test and live in berlin" do
+        auth_headers = @registered_user.create_new_auth_token
+        request.headers.merge!(auth_headers)
+
+        get :search, {page: "1", :text => "first_test berlin"}, format: :json
+
+        expect(response.code).to eq "200"
+        expect(json.length).to eq 1
 
       end
 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -28,6 +28,22 @@ FactoryGirl.define do
 
     end
 
+    trait :personal_programm_data_diff do
+
+      first_name 'first_test'
+      last_name 'last_test'
+      country 'DE'
+      city 'Köln'
+      date_of_birth Date.new(1995, 12, 3)
+      gender 0
+
+      program_type 0
+      institution 'example institutation'
+      year_of_participation 2006
+      country_of_participation 'DE'
+      student_company_name 'company name'
+
+    end
 
     trait :confirmed_email do
         confirmed_at Time.now
