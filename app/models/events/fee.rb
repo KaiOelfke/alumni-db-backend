9f6a9afb22471ee8a3d6d74645c3061ab8dcb8e2
Kai Oelfke <k.oelfke@me.com>
2016-07-21 14:02:58 +0200

    Fix failing testcases by adding boolean fields to factory, remove presence validation as it fails for booleans with false value

--- app/models/events/fee.rb
+++ app/models/events/fee.rb
@@ -5,7 +5,7 @@ class Events::Fee < ActiveRecord::Base
 
   belongs_to :event
 
-	validates :event, :name, :price, :deadline, :early_bird_fee, :honoris_fee, presence: true
+	validates :event, :name, :price, :deadline, presence: true
   validates :name, length: {minimum: 3}
 	validates :delete_flag, :early_bird_fee, :honoris_fee, inclusion: { in: [true, false] }
   validates :price, numericality: true
--- spec/factories/events/fee.rb
+++ spec/factories/events/fee.rb
@@ -3,8 +3,9 @@ FactoryGirl.define do
     name "MyString"
     price 1
     deadline Date.current + 1.month
-    association :event, factory: :event, strategy: :build
-
+    early_bird_fee false
+    honoris_fee false
+    association :event, factory: :event, strategy: :build 
     trait :fee_codes do
     	fee_codes {[FactoryGirl.create(:fee_code),
     							FactoryGirl.create(:fee_code),
